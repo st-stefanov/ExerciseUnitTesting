4c69c666158308f70a8e6602a73c6e496e177327
lenovo <stanimir_cena@abv.bg>
2024-08-12 21:13:31 +0300

    Add project files.

---
+++ TestApp.UnitTests/PrimeFactorTests.cs
@@ -0,0 +1,36 @@
+﻿using NuGet.Frameworks;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PrimeFactorTests
+{
+    [Test]
+    public void Test_FindLargestPrimeFactor_NumberLowerThanTwo()
+    {
+        long n = 1;
+
+        Assert.That(() => PrimeFactor.FindLargestPrimeFactor(n), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_PrimeNumber()
+    {
+        long n = 5;
+
+        long result = PrimeFactor.FindLargestPrimeFactor(n);
+
+        Assert.That(result, Is.EqualTo(n));
+    }
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_LargeNumber()
+    {
+        long n = 123456789123456789;
+
+        long result = PrimeFactor.FindLargestPrimeFactor(n);
+
+        Assert.That(result, Is.EqualTo(52579));
+    }
+}
---
+++ TestApp.UnitTests/ReverserTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ReverserTests
+{
+    [Test]
+    public void Test_ReverseStrings_WithEmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        string[] inputArray = Array.Empty<string>();
+
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_ReverseStrings_WithSingleString_ReturnsReversedString()
+    {
+        // Arrange
+        string[] inputArray = new string[] { "Hello" };
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "olleH" }));
+    }
+
+    [Test]
+    public void Test_ReverseStrings_WithMultipleStrings_ReturnsReversedStrings()
+    {
+        // Arrange
+        string[] inputArray = new string[] { "Hello", "Bye"};
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "olleH", "eyB" }));
+    }
+
+    [Test]
+    public void Test_ReverseStrings_WithSpecialCharacters_ReturnsReversedSpecialCharacters()
+    {
+        // Arrange
+        string[] inputArray = new string[] { "Hello!", "Bye." };
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "!olleH", ".eyB" }));
+    }
+}
